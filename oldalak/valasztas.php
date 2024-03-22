3b3a5bdba2ca6d471752dd944c7249deaec629f2
Kurucz Laci <lacikurucz09@gmail.com>
2024-03-22 19:51:57 +0100

    valasztas created

---
+++ oldalak/valasztas.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="hu">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <title>Választás</title>
+</head>
+
+<body>
+
+
+
+    <form action="szamolas.php">
+
+        <label for="oktato">Oktató:</label>
+        <input type="text" id="oktato">
+        <br>
+        <label for="word">Szó(fordulat):</label>
+        <input type="text" id="word">
+
+        <input type="submit" value="Kezdés">
+
+    </form>
+
+
+
+
+</body>
+
+</html>
