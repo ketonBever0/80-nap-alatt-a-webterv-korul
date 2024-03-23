f61f67fe5af5b65fbae69ada94bdbddf6055c053
rbrt1022 <rbrt1022.it@outlook.hu>
2024-03-23 15:44:59 +0100

    Telefonos menüsor formázása, illetve a hambimenü gomb funkciója

---
+++ JS/mobilmenu.js
@@ -0,0 +1,17 @@
+function nyisdKi() {
+    let menu = document.getElementById("opcio");
+    menu.style.display = "block";
+    
+    let hambigomb = document.getElementById("hambi");
+    //hambigomb.style.backgroundColor = "blue";
+    hambigomb.style.display = "none";
+}
+
+function csukdBe() {
+    let menu = document.getElementById("opcio");
+    menu.style.display = "none";
+    
+    let hambigomb = document.getElementById("hambi");
+    //hambigomb.style.backgroundColor = "blue";
+    hambigomb.style.display = "block";
+}
